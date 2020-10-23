32ca4e530184938c82b30b45a6840d4c8ce6a8d6
Marius <marius.gutoi@gmail.com>
2020-10-23 12:32:58 +0300

    improvements for coordinator

--- AppStack/Classes/BaseNavigators/BaseNavigator.swift
+++ AppStack/Classes/BaseNavigators/BaseNavigator.swift
@@ -13,6 +13,8 @@ public protocol BaseNavigator {
     
     var baseController: VC! { get }
     
+    var rootViewController: UIViewController! { get }
+    
     init(baseController: VC)
     
     func buildRoot() -> UIViewController
@@ -22,17 +24,25 @@ extension BaseNavigator where VC: UINavigationController {
     func setRoot() {
         baseController.viewControllers = [buildRoot()]
     }
+    
+    public func endFlow(_ animated: Bool = true) {
+        var viewControllers = baseController.viewControllers
+        guard let rootIndex = viewControllers.firstIndex(of: rootViewController) else { fatalError() }
+        viewControllers.removeSubrange(rootIndex...)
+        baseController.setViewControllers(viewControllers, animated: animated)
+    }
 }
 
 open class BaseNavigatorImplementation<VC: UIViewController>: BaseNavigator {
     public weak var baseController: VC!
+    public weak var rootViewController: UIViewController!
     
     public required init(baseController: VC) {
         self.baseController = baseController
     }
     
     open func buildRoot() -> UIViewController {
-        UIViewController()
+        fatalError()
     }
 }
 
