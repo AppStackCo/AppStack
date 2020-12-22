1aa92cbf3a6935bc49b891384aba6377c2dbe742
Alin Popa <alinmrpopa@gmail.com>
2020-12-22 13:01:39 +0200

    Moved the tableView&dataSource setup in the UIView extensions file

--- AppStack/Classes/ViewExtensions/UIView+Extensions.swift
+++ AppStack/Classes/ViewExtensions/UIView+Extensions.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2020 AppStack. All rights reserved.
 //
 
-import UIKit
+import RxDataSources
+import RxSwift
 
 public struct AnchorPadding {
     let top: CGFloat
@@ -56,5 +57,39 @@ extension UIView {
                 completionHandler?()
             }
         }
+    }
+    
+    public func setupTableViewDataSource(tableView: UITableView, cellIdentifiers: [String], dataSource: inout RxTableViewSectionedReloadDataSource<TableViewSectionModel>?, disposeBag: DisposeBag) {
+        tableView.setup()
+        
+        cellIdentifiers.forEach { cellIdentifier in
+            tableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        }
+        
+        let configureCell: RxTableViewSectionedReloadDataSource<TableViewSectionModel>.ConfigureCell = { (_, tableView, indexPath, cellModel) in
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellModel.identifier, for: indexPath)
+            (cell as? BaseTableViewCell)?.update(using: cellModel)
+            
+            return cell
+        }
+        
+        let canEditRowAtIndexPath: RxTableViewSectionedReloadDataSource<TableViewSectionModel>.CanEditRowAtIndexPath = { (_, _) in false }
+        
+        dataSource = .init(configureCell: configureCell,
+                           canEditRowAtIndexPath: canEditRowAtIndexPath)
+        
+        tableView.rx.modelSelected(BaseTableViewCellViewModel.self)
+            .subscribe(onNext: { model in
+                (model as? SelectableTableViewCellViewModel)?.selectedAction.execute()
+            })
+            .disposed(by: disposeBag)
+        
+        // This adds the visual effect of de-selecting a row once touchUp has been fired.
+        tableView.rx
+            .itemSelected
+            .subscribe(onNext: { indexPath in
+                tableView.deselectRow(at: indexPath, animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 }
