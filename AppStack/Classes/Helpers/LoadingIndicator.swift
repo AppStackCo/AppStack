92c030ca0bf0738968e3b2b9912c2ade69c6b588
Alin Popa <alinmrpopa@gmail.com>
2021-01-24 13:41:30 +0200

    Exposed Alert-related classes and customised the LoadingView in order to receive different animations from consumers.

--- AppStack/Classes/Helpers/LoadingIndicator.swift
+++ AppStack/Classes/Helpers/LoadingIndicator.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2020 AppStack. All rights reserved.
 //
 
+import Lottie
 import RxCocoa
 import RxSwift
-import UIKit
 
 public class LoadingIndicator {
     private enum LoadingStatus {
@@ -16,10 +16,12 @@ public class LoadingIndicator {
         case present
     }
     
-    public static let shared = LoadingIndicator()
     private let statusSubject = PublishRelay<LoadingStatus>()
     private let loadingView = LoadingView(frame: CGRect.zero)
+    
     let disposeBag = DisposeBag()
+    
+    public static let shared = LoadingIndicator()
     
     private init() {
         statusSubject
@@ -50,6 +52,22 @@ public class LoadingIndicator {
             })
             .subscribe()
             .disposed(by: disposeBag)
+    }
+    
+    /**
+     Will set a custom animation given the name of it and the bundle where it is located.
+     - Parameter animationName: The name of the lottie animation.
+     - Parameter animationBundle: An optional  bundle in which the animation .json file can be located.
+     - Parameter contentMode: The content mode of the animation..
+     - Parameter loopMode: The loop mode of the animation..
+     */
+    public func setupAnimation(animationName: String,
+                               animationBundle: Bundle? = nil,
+                               contentMode: UIView.ContentMode = .scaleAspectFill,
+                               loopMode: LottieLoopMode = .loop) {
+        loadingView.animation = Animation.named(animationName)
+        loadingView.animationContentMode = contentMode
+        loadingView.animationLoopMode = loopMode
     }
     
     public func presentLoadingIndicator() {
