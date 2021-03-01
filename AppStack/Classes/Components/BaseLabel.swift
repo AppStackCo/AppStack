c6f348c161b3ad13b6a7ad0384d9a2bb68b20fa8
Alin Popa <alinmrpopa@gmail.com>
2021-03-01 08:36:28 +0200

    Fixed the color setup of BaseLabel

--- AppStack/Classes/Components/BaseLabel.swift
+++ AppStack/Classes/Components/BaseLabel.swift
@@ -10,8 +10,8 @@ import UIKit
 public class BaseLabel: UILabel {
     
     public override var text: String? {
-        didSet{
-            attributedText = textStyle.attributedText(for: text ?? "", newColor: textColor)
+        didSet {
+            attributedText = textStyle.attributedText(for: text ?? "", newColor: textStyle.color)
         }
     }
     
